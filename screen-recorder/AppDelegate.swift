079df9d8667a61c339aa83e87bbf5d4ed186db4e
Ben Tranter <bwtrante@lakeheadu.ca>
2015-06-15 22:43:38 -0400

    Initial Commit

---
+++ screen-recorder/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  screen-recorder
+//
+//  Created by Ben Tranter on 15/06/15.
+//  Copyright (c) 2015 Ben Tranter. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
