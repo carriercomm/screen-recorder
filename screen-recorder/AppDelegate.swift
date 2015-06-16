6463d52472e981a87bcecfb988281fe1e53ee88b
Ben Tranter <bwtrante@lakeheadu.ca>
2015-06-15 23:03:08 -0400

    Configure status bar image

--- screen-recorder/AppDelegate.swift
+++ screen-recorder/AppDelegate.swift
@@ -12,10 +12,13 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
-
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-2)
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        if let button = statusItem.button {
+            button.image = NSImage(named: "StatusBarButtonImage")
+            button.action = Selector("printQuote")
+        }
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
